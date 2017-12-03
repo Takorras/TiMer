7460483a75668b078ad568a71c852c3353502dc0
Takorras <sy.empress0321@gmail.com>
2017-12-03 15:20:46 +0900

    [modify]styledを適用

--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -2,30 +2,29 @@ import React from 'react'
 import moment from 'moment'
 import styled from 'styled-components'
 
+const Wrapper = styled.div`
+  text-align: center;
+`
+
+const Dating = styled.h1`
+  font-size: 4.8em;
+  margin-bottom: 0;
+`
+
+const Day = styled.h1`
+  font-size: 2.4em;
+  margin: 0;
+`
+
+const Time = styled.h1`
+  font-size: 7.2em;
+  margin: 16px;
+`
+
 class Clock extends React.Component {
 
   render(){
-
     const m = moment()
-
-    const Wrapper = styled.div`
-      text-align: center;
-    `
-
-    const Dating = styled.h1`
-      font-size: 4.8em;
-      margin-bottom: 0;
-    `
-
-    const Day = styled.h1`
-      font-size: 2.4em;
-      margin: 0;
-    `
-
-    const Time = styled.h1`
-      font-size: 7.2em;
-      margin: 16px;
-    `
 
     return (
       <Wrapper>
